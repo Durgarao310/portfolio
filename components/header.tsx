9b62862f85cd34cb81baa249079fe61b87518f5f
Durga rao Ganta <durgaraoganta@Durgas-MacBook-Pro.local>
2025-01-26 23:45:11 +0530

    latest

--- components/header.tsx
+++ components/header.tsx
@@ -11,9 +11,9 @@ export default function Header() {
     useActiveSectionContext();
 
   return (
-    <header className='z-[999] relative'>
-      <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 bg-slate-900 px-6 rounded-full'>
-        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-white sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+    <header className='z-[999] relative w-full'>
+      <nav className='fixed top-0 lg:top-1 md:top-1 sm:top-1 w-full flex items-center justify-center'>
+        <ul className='bg-slate-900 lg:rounded-full md:rounded-full sm:rounded-full flex flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-white sm:w-[initial] sm:flex-nowrap sm:gap-5'>
           {links.map((link) => (
             <li
               className='h-3/4 flex items-center justify-center relative'
